f01aa179d5383c06127c698b8143de9f7a02769c
nospam <nospam>
2022-06-02 10:39:09 +0200

    new: SystemProperties.getHomeDir()

--- main/java/com/eriklievaart/toolkit/io/api/SystemProperties.java
+++ main/java/com/eriklievaart/toolkit/io/api/SystemProperties.java
@@ -1,5 +1,7 @@
 package com.eriklievaart.toolkit.io.api;
 
+import java.io.File;
+
 public class SystemProperties {
 	private static final String OS = System.getProperty("os.name").toLowerCase();
 
@@ -21,5 +23,9 @@ public class SystemProperties {
 
 	public static boolean isUnix() {
 		return OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0;
+	}
+
+	public static File getHomeDir() {
+		return new File(System.getProperty("user.home"));
 	}
 }
