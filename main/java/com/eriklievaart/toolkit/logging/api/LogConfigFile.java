1f7dc2573210073f84775089a41810b26b566d2b
nospam <nospam>
2023-05-23 10:02:37 +0200

    new: init logging from directory

--- main/java/com/eriklievaart/toolkit/logging/api/LogConfigFile.java
+++ main/java/com/eriklievaart/toolkit/logging/api/LogConfigFile.java
@@ -3,15 +3,39 @@ package com.eriklievaart.toolkit.logging.api;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 
+import com.eriklievaart.toolkit.io.api.CheckFile;
+import com.eriklievaart.toolkit.io.api.Console;
 import com.eriklievaart.toolkit.io.api.LineFilter;
 import com.eriklievaart.toolkit.io.api.RuntimeIOException;
 import com.eriklievaart.toolkit.lang.api.AssertionException;
+import com.eriklievaart.toolkit.lang.api.collection.ListTool;
 import com.eriklievaart.toolkit.logging.api.level.LogLevelTool;
 
 public class LogConfigFile {
+	private static List<String> locations = ListTool.of("logging.ini", "logging.properties");
+
+	public static void initFromDirectory(File directory) {
+		getOptionalInitFile(directory).ifPresent(LogConfigFile::load);
+	}
+
+	private static Optional<File> getOptionalInitFile(File directory) {
+		for (String location : locations) {
+			File file = new File(directory, location);
+			if (file.isFile()) {
+				return Optional.of(file);
+			}
+		}
+		Console.println("\nmissing log config file!\n$ $\n", directory, locations);
+		return Optional.empty();
+	}
+
 	public static void load(File file) {
+		CheckFile.isFile(file);
+		System.out.println("using log config file: " + file);
 		try (InputStream is = new FileInputStream(file)) {
 			load(is);
 
