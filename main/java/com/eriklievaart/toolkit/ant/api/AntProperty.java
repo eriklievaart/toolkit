4eaae8c4e464c2431c19034de3bff9a440be2ba6
nospam <nospam>
2026-02-06 09:48:10 +0100

    new: load ant property or file from home dir

--- main/java/com/eriklievaart/toolkit/ant/api/AntProperty.java
+++ main/java/com/eriklievaart/toolkit/ant/api/AntProperty.java
@@ -3,7 +3,9 @@ package com.eriklievaart.toolkit.ant.api;
 import java.io.File;
 import java.io.IOException;
 
+import com.eriklievaart.toolkit.io.api.CheckFile;
 import com.eriklievaart.toolkit.io.api.RuntimeIOException;
+import com.eriklievaart.toolkit.io.api.SystemProperties;
 import com.eriklievaart.toolkit.lang.api.check.Check;
 import com.eriklievaart.toolkit.lang.api.check.CheckStr;
 import com.eriklievaart.toolkit.lang.api.str.Str;
@@ -33,16 +35,28 @@ public class AntProperty {
 		CheckStr.notBlank(fallback);
 
 		try {
-			if (Str.isBlank(property)) {
-				File local = new File(fallback);
-				String root = new File("").getCanonicalPath();
-				Check.isTrue(local.isDirectory(), "% is not a directory in $", fallback, root);
-				return local;
+			if (Str.notBlank(property)) {
+				return getDirectory();
 			}
-			return getDirectory();
+			File local = new File(fallback);
+			String root = new File("").getCanonicalPath();
+			Check.isTrue(local.isDirectory(), "% is not a directory in $", fallback, root);
+			return local;
+
 		} catch (IOException e) {
 			throw new RuntimeIOException(e);
 		}
+	}
+
+	public File getDirectoryOrHomeDir(String path) {
+		CheckStr.notBlank(path);
+
+		if (Str.notBlank(property)) {
+			return getDirectory();
+		}
+		File fallback = new File(SystemProperties.getHomeDir(), path);
+		CheckFile.isDirectory(fallback);
+		return fallback;
 	}
 
 	private void check() {
